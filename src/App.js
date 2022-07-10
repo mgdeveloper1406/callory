59d098fc5ff51855177410f8a262070226b10f4f
MG_Apps <99666081+mgdeveloper1406@users.noreply.github.com>
2022-07-10 12:06:06 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://calorietrackermiri.netlify.app/">
-        Dwan W.
+        
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
